5618dc177b1c68cb4ae11b4f4afb4b447b7d8051
Rafael Gonçalves <rafagonc77@yahoo.com.br>
2016-04-16 17:49:03 -0300

    endpoints updated

--- endpoints/__init__.py
+++ endpoints/__init__.py
@@ -1,11 +1,13 @@
 __author__ = 'rafagonc'
 from .rating import RatingBookRequest
+from .book import  TopRatedBooksRequest
 from flask import Blueprint
 from .api import api
 
 def create_blueprint():
     api.init_app(blueprint)
     api.add_resource(RatingBookRequest, '/rating')
+    api.add_resource(TopRatedBooksRequest, '/book/top')
 
 blueprint = Blueprint('endpoints', __name__)
 create_blueprint()
