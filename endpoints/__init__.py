b5dc6b96b040c9090eba2f02b42da69e200ac5a0
Rafael Gonçalves <rafagonc77@yahoo.com.br>
2016-05-08 00:40:27 -0300

    endpoints added

--- endpoints/__init__.py
+++ endpoints/__init__.py
@@ -3,15 +3,18 @@ from .rating import RatingBookRequest
 from .book import  TopRatedBooksRequest
 from .book import BookEndpoint
 from .log import LogEndpoint
+from .user import UserEndpoint
 from flask import Blueprint
 from .api import api
 
 def create_blueprint():
     api.init_app(blueprint)
+    api.add_resource()
     api.add_resource(RatingBookRequest, '/rating')
     api.add_resource(TopRatedBooksRequest, '/book/top')
     api.add_resource(BookEndpoint, '/book')
     api.add_resource(LogEndpoint, '/log')
+    api.add_resource(UserEndpoint, '/user')
 
 
 blueprint = Blueprint('endpoints', __name__)
