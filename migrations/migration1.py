3623d94476916cfab792850e885c058ebbf8f6da
Rafael Gonçalves <rafagonc77@yahoo.com.br>
2016-05-23 09:15:51 -0300

    migration

--- migrations/migration1.py
+++ migrations/migration1.py
@@ -3,8 +3,8 @@ import psycopg2
 import psycopg2.extras
 import json
 
-#url = "https://reading-list-stage.herokuapp.com/"
-url = "http://192.168.1.31:5000/"
+url = "https://reading-list-stage.herokuapp.com/"
+#url = "http://192.168.1.31:5000/"
 
 #connect to postgres
 connect_addr = "host='localhost' dbname='reading-list-backup'"
@@ -17,7 +17,9 @@ cursor = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
 cursor.execute("SELECT BOO.name as book_name, AUT.name as author_name, CAT.name as category_name, RAT.rating as rating FROM red_book as BOO INNER JOIN red_author as AUT ON BOO.author_id = AUT.id INNER JOIN red_category as CAT ON BOO.category_id = CAT.id JOIN red_rating as RAT ON BOO.id = RAT.book_id;")
 
 up = []
-for book_dict in cursor.fetchall():
+book_dicts = cursor.fetchall()[601::]
+print(book_dicts)
+for book_dict in book_dicts:
     book = dict(book_name=book_dict['book_name'], author_name=book_dict['author_name'], category_name=book_dict['category_name'], rating=book_dict['rating'])
     up.append(book)
 
