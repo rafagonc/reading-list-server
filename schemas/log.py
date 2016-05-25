53168062d50aab201543bca87c7af67773f81f5c
Rafael Gonçalves <rafagonc77@yahoo.com.br>
2016-05-24 21:50:42 -0300

    log fix

--- schemas/log.py
+++ schemas/log.py
@@ -3,10 +3,10 @@
 from ma import ma
 from models.log import ReadingLog
 from marshmallow import fields
-from book import BookSchema
+from user_book import UserBookSchema
 
 class LogSchema(ma.ModelSchema):
-    book_ref = fields.Nested(BookSchema)
+    book_ref = fields.Nested(UserBookSchema)
 
     class Meta:
         fields = ['pages', 'date', 'user', 'book_ref', 'id', 'uuid']
