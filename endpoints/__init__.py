2cc549645d5d698f3f959cadbcfd2136d3acd007
Rafael Gonçalves <rafagonc77@yahoo.com.br>
2016-06-24 01:28:56 -0300

    note endpoint

--- endpoints/__init__.py
+++ endpoints/__init__.py
@@ -6,6 +6,7 @@ from .book import  TopRatedBooksRequest
 from .rating import MultipleRatingBookRequest
 from .book import BookEndpoint
 from .log import LogEndpoint
+from .note import NoteEndpoint
 from .user import UserEndpoint
 from flask import Blueprint
 from .api import api
@@ -15,6 +16,7 @@ def create_blueprint():
     api.add_resource(RatingBookRequest, '/rating')
     api.add_resource(MultipleRatingBookRequest, '/mrating')
     api.add_resource(TopRatedBooksRequest, '/book/top')
+    api.add_resource(NoteEndpoint, '/note')
     api.add_resource(BookEndpoint, '/book')
     api.add_resource(LogEndpoint, '/log')
     api.add_resource(UserEndpoint, '/user')
